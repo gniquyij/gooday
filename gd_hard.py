6f8e3739fe375b7a86b64d363c705db4791ceb59
yuqing.ji <yuqing.ji@outlook.com>
2020-05-03 10:08:43 +0800

    update

--- gd_hard.py
+++ gd_hard.py
@@ -23,26 +23,27 @@ def interact(tip):
     return input('%s: ' % tip)
 
 
-def guess(sentence, time=2):
-    word, sentence_guess = pick_word(sentence)
-    print('\n%s\n' % sentence_guess)
-    guess = interact('Guess')
-    n = 0
-    while n < time:
+def trick(word, sentence, n):
+    if n == 0:
+        guess = interact('Last chance')
+        if guess != word:
+            print('\n%s\n' % sentence)
+            print('%s\n' % 'LEARN THEN HIT IT NEXT TIME!')
+            return
+        print('\n%s\n' % 'YOU GOT IT!')
+    if n > 0:
+        guess = interact('Guess')
         if guess == word:
             print('\n%s\n' % 'YOU GOT IT!')
             return
-        n += 1
-        interact('Try again')
-        if n == time:
-            interact('Last chance')
-    print('\n%s\n' % sentence)
-    print('%s\n' % 'LEARN THEN HIT IT NEXT TIME!')
+        return trick(word, sentence, n-1)
 
 
 def main(file):
     quote = gd.get_quote_from_local(file)[0]
-    guess(quote)
+    word, quote_guess = pick_word(quote)
+    print('\n%s\n' % quote_guess)
+    trick(word, quote, 2)
 
 
 if __name__ == '__main__':
